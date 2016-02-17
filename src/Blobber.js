ca907c89fea5becd5abd9780269371d2ab6c2ac8
Leon Chen <lchen3@gmail.com>
2016-02-17 17:02:18 -0500

    svg and container styles as props

--- src/Blobber.js
+++ src/Blobber.js
@@ -3,10 +3,12 @@ import { rect2rectPoints, orthoConvexHull, roundedSVGPath } from './lib/funcs';
 
 import './Blobber.css';
 
-const Blobber = ({ rects, pathOffset, cornerRadius }) => {
+const Blobber = ({ rects, pathOffset, cornerRadius, containerStyle, svgStyle }) => {
   const rectsAsPoints = rects.map(rect => rect2rectPoints(rect));
   const hull = orthoConvexHull(rectsAsPoints, pathOffset);
   const roundedHullStr = roundedSVGPath(hull, cornerRadius);
+
+  const { fill, opacity, stroke } = svgStyle || {};
 
   let svgPointsStr = '';
   for (let i = 0; i < hull.length; i++) {
@@ -18,11 +20,14 @@ const Blobber = ({ rects, pathOffset, cornerRadius }) => {
   );
 
   return(
-    <div className='svgContainer' style={{position:'absolute', left:'0px', top:'0px', width:'100%', height:'100%'}}>
+    <div
+      className='svgContainer'
+      style={containerStyle || { position:'absolute', left:'0px', top:'0px', width:'100%', height:'100%' }}
+    >
       <svg width='100%' height='100%'>
         {/*svgRects*/}
         {/*<polyline fill='none' opacity='.25' stroke='blue' points={svgPointsStr}/>*/}
-        <path fill='purple' opacity='.5' stroke='black' d={roundedHullStr} />
+        <path fill={fill || 'lightgray'} opacity={opacity || '.5'} stroke={stroke || 'lightgray'} d={roundedHullStr} />
       </svg>
     </div>
   );
