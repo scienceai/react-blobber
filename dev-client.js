3d32958fa0c44bb4533d15ae593de1ae0d918ae4
Leon Chen <lchen3@gmail.com>
2016-02-17 17:17:30 -0500

    comment out console.log

--- dev-client.js
+++ dev-client.js
@@ -32,18 +32,18 @@ class Main extends React.Component {
     //document.removeEventListener('onmouseup', this.dragEnd.bind(this));
   }
 
-  dragStart(group, i, event){
+  dragStart(group, i, event) {
     //console.log('mousedown', i, event);
     this.setState({dragGroup: group, dragging: i});
   }
 
-  dragEnd(event){
-    console.log('dragEnd');
+  dragEnd(event) {
+    //console.log('dragEnd');
     this.setState({dragging: -1});
   }
 
-  handleMouseMove(e){
-    console.log('drag: ', this.state.dragging);
+  handleMouseMove(e) {
+    //console.log('drag: ', this.state.dragging);
     if (this.state.dragGroup == -1 || this.state.dragging == -1) return;
     var newRects = this.state.rectGroups.slice();
     newRects[this.state.dragGroup][this.state.dragging].x = e.pageX-30;
@@ -56,29 +56,40 @@ class Main extends React.Component {
      e.preventDefault();
   }
 
-  render(){
-    let rectDivs = this.state.rectGroups.map((rectGroup, i) => {
-      let cells = rectGroup.map((rect, j) => {
+  render() {
+
+    const labels = [
+      ['Apple', 'Google', 'Facebook'],
+      ['Cat', 'Dog', 'Sthenurine'],
+    ];
+
+    const rectDivs = this.state.rectGroups.map((rectGroup, i) => {
+      const cells = rectGroup.map((rect, j) => {
         //console.log('rect ', rect);
-        let width = rect.width;
-        let height = rect.height;
-        let top = rect.y;
-        let left = rect.x;
+        const width = rect.width;
+        const height = rect.height;
+        const top = rect.y;
+        const left = rect.x;
         //console.log('rect loc ', width, height, top, left);
-        let divStyle = {top:top, left:left, width:width, height:height, zIndex:100};
+        const divStyle = {top:top, left:left, width:width, height:height, zIndex:100};
         return(
           <div className={'cell group_' + i} key={'group_' + i + '_' + j} style={divStyle}  onMouseDown={this.dragStart.bind(this, i, j)} onMouseUp={this.dragEnd.bind(this)}>
-            <div className='label'> label</div>
+            <div className='label'>{labels[i][j]}</div>
           </div>
         );
       });
       return cells;
     });
 
-    let blobbers = this.state.rectGroups.map((rectGroup, i)=> {
-      console.log(i);
+    const blobColors = ['#26A65B', '#F89406'];
+    const blobbers = this.state.rectGroups.map((rectGroup, i)=> {
       return(
-        <Blobber key={'blobber_' + i} rects={rectGroup} cornerRadius={16} pathOffset={4}/>
+        <Blobber
+          key={'blobber_' + i}
+          rects={rectGroup}
+          cornerRadius={16}
+          pathOffset={4}
+          svgStyle={{ fill: blobColors[i], stroke: blobColors[i] }} />
       );
     });
 
