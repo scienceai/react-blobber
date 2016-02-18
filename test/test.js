2e5854b4ce3851a96486cb3164fb62eecf2ee8f5
Leon Chen <lchen3@gmail.com>
2016-02-18 11:37:57 -0500

    update test

--- test/test.js
+++ test/test.js
@@ -3,10 +3,16 @@ import { shallow } from 'enzyme';
 import assert from 'assert';
 import Blobber from '../src/Blobber';
 
+const rects = [
+  { x: 30, y: 250, width: 150, height: 24 },
+  { x: 100, y: 285, width: 150, height: 24 },
+  { x: 200, y: 310, width: 150, height: 24 },
+];
+
 describe('Blobber', () => {
 
   it('renders the <Blobber /> component', () => {
-    const wrapper = shallow(<Blobber />);
+    const wrapper = shallow(<Blobber rects={rects}/>);
     assert(wrapper.find('svg'));
   });
 
